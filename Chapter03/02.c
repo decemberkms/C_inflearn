66ec70aaaa302ead4ce2bb5d41e31373aba4f96a
decemberkms <decemberkms@gmail.com>
2021-10-10 21:28:46 +0200

    "Add Chapter03 1"

---
+++ Chapter03/02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // operator   // operand
+    // i (operand)  
+
+    // Data object (object)
+    // Data inside memory -> object 
+    // L-value  left value
+    // R-value  right value
+    // r value can act as r value
+
+    // i = 1024; 
+    // 1024 is not a data object 
+    // i is data object
+    // L -> object locator value  memory address's name
+    // R -> value of an expression   literal
+
+
+
+
+    return 0;
+}
---
+++ Chapter03/03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int income, salary, bonus;
+
+    income = salary = bonus = 100;
+
+    printf("%d\n", income);
+
+    // 1.0f + 2; -> float -> promotion
+
+    // binary operator   3-2
+
+    // unary operator   -15
+
+
+    return 0;
+}
