a0fc8b66ad88225ed8d8325cc51c886332a3ca73
decemberkms <decemberkms@gmail.com>
2021-10-24 17:02:28 +0200

    Add till array and pointer

--- Chapter08/03.c
+++ Chapter08/03.c
@@ -16,7 +16,35 @@
 
 int main()
 {   
+
+    int arr[10];
+
+    const int num = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < num; ++i)
+        arr[i] = (i + 1) * 100;
     
+    int* ptr = arr;
+
+    printf("%p %p %p\n", ptr, arr, &arr[0]);
+
+    // ptr += 2;
+
+    printf("%p %p %p\n", ptr, arr + 2, &arr[2]);
+
+
+    // Nte : arr += 2;// invalid
+
+    printf("%d %d %d\n", *(ptr + 1), *(arr + 3), arr[3]);
+
+    // int* ptr = NULL;
+    // ptr = arr;
+
+    for (int i = 0; i < num; ++ i)
+    {
+        printf("%d %d\n", *ptr++, arr[i]);
+    }
+
+
     return 0; 
 }
 
