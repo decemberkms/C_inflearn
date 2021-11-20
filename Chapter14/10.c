dc83d487428f696fc5bdb6473620a487d5c13d8a
decemberkms <decemberkms@gmail.com>
2021-11-20 20:09:43 +0100

    Finish Chapter14

---
+++ Chapter14/10.c
@@ -0,0 +1,14 @@
+#include "MyLibrary.h"
+
+#include <stdio.h>
+
+void say_hello()
+{
+    printf("Hello\n");
+}
+
+
+void say_world()
+{
+    printf("World\n");
+}
--- Chapter14/11.c
+++ Chapter14/11.c
@@ -7,40 +7,17 @@
 #include <stdbool.h>
 #include <complex.h>
 #include <string.h> // strlen(), strcmp()
-#include <math.h>
 #include <stdbool.h>
 #include <ctype.h>  // character functions
 #include <iso646.h>  // not used so often   -- and or not
 #include <stdlib.h> //srand()   //malloc(), free()
 
-/*
-    
-    Function call has overhead
-    - set up the call, pass arguments, jump to the function code
-    and return
-
-    inline function
-    - suggests inline replacements
-
-    inline functions should be short
-    function with internal linkeage can be made inline (GCC clang)
-    you can't take its address
-    
-
-*/
-
-static inline  int foo()
-{
-    return 5;
-}
+#include <math.h>
 
 int main()
 {     
-    int ret;
+    printf("%f\n", cos(3.141592));
 
-    ret = foo();
-
-    printf("Output is: %d\n", ret);
-
+    
     return 0; 
 }
